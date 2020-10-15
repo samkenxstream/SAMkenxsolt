8ea8b75521a84e13e982cdb96513cd929f7f14f7
Jubb <hjubb93@gmail.com>
2020-10-16 00:46:07 +1000

    I am looking directly at the output

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     kotlin("multiplatform") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
 }
 group = "com.github.hjubb.solinput"
 version = "0.1.0"
 
 repositories {
     mavenCentral()
-    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
+    maven {
+        setUrl("https://kotlin.bintray.com/kotlinx")
+    }
+    maven {
+        setUrl("https://repo.binom.pw/releases")
+    }
 }
 kotlin {
     macosX64 {
@@ -36,8 +42,9 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-io:0.1.16")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0-M1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
+                api("pw.binom.io:file:0.1.20")
+                api("pw.binom.io:core:0.1.20")
             }
         }
         val commonTest by getting
--- src/commonMain/kotlin/com/github/hjubb/solinput/main.kt
+++ src/commonMain/kotlin/com/github/hjubb/solinput/main.kt
@@ -1,5 +1,102 @@
 package com.github.hjubb.solinput
 
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Polymorphic
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.*
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.contextual
+import kotlinx.serialization.modules.polymorphic
+import pw.binom.ByteBuffer
+import pw.binom.ByteBufferPool
+import pw.binom.Input
+import pw.binom.io.AsyncBufferedOutput
+import pw.binom.io.BufferedInput
+import pw.binom.io.file.*
+import pw.binom.io.readText
+import pw.binom.io.utf8Reader
+import pw.binom.toByteBufferUTF8
+
 fun main(args: Array<String>) {
-    println("howdy")
+    val parser = ArgParser("sol-input")
+    val dir by parser.option(ArgType.String, shortName = "d", description = "root directory for file search").required()
+    parser.parse(args)
+    val files = collectFiles(getDir(dir))
+    val sol = process(files)
+    val solString = Json {
+        // any Json config here
+    }.encodeToString(sol)
+    File("solc-input.json").write(append = false).write(solString.toByteBufferUTF8())
 }
+
+fun process(files: List<WrappedFile>): BigSolInput {
+    val content = files.asSequence().associate { it.path to it.getContent() }
+    return BigSolInput(language = "Solidity",
+            sources = content,
+            settings = buildJsonObject {
+                put("metadata", buildJsonObject {
+                    put("useLiteralContent", true)
+                })
+                put("optimizer", buildJsonObject {
+                    put("enabled", true)
+                    put("runs", 200)
+                })
+                put("outputSelection", buildJsonObject {
+                    put("*", buildJsonObject {
+                        put("*", buildJsonArray {
+                            add("abi")
+                            add("evm.bytecode")
+                            add("evm.deployedBytecode")
+                            add("evm.methodIdentifiers")
+                        })
+                        put("", buildJsonArray {
+                            add("id")
+                            add("ast")
+                        })
+                    })
+                })
+            }
+    )
+}
+
+fun collectFiles(dir: File): List<WrappedFile> {
+    val files = mutableListOf<WrappedFile>()
+    val regex = Regex("(?:\\.[\\\\|/])?([\\w|/\\\\]+\\.sol)")
+    for (file in dir.iterator()) {
+        if (file.isDirectory) {
+            files += collectFiles(file)
+        }
+        regex.matchEntire(file.path)?.groups?.get(1)?.let { matchGroup ->
+            val path = matchGroup.value
+            files += WrappedFile(path, file)
+        }
+    }
+    return files
+}
+
+data class WrappedFile(val path: String, val file: File) {
+    fun getContent(): Content {
+        val content = BufferedInput(file.read()).utf8Reader().readText()
+        return Content(content)
+    }
+}
+
+fun getDir(dir: String): File {
+    return File(dir)
+}
+
+@Serializable
+data class Content(val content: String)
+
+@Serializable
+data class BigSolInput(
+        val language: String,
+        val sources: Map<String, Content>,
+        val settings: JsonObject
+)
